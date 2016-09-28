8c031156c6af8194698820bd1236352a1dd8c0eb
tomaszcib <ciborski.t@gmail.com>
2016-09-28 20:41:14 +0200

    Add files via upload

---
+++ bin/gnulinux-build/moc_mainbox.cpp
@@ -0,0 +1,395 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainbox.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.6.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../src/mainbox.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainbox.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.6.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainBox_t {
+    QByteArrayData data[72];
+    char stringdata0[710];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainBox_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainBox_t qt_meta_stringdata_MainBox = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "MainBox"
+QT_MOC_LITERAL(1, 8, 7), // "drawMap"
+QT_MOC_LITERAL(2, 16, 0), // ""
+QT_MOC_LITERAL(3, 17, 1), // "t"
+QT_MOC_LITERAL(4, 19, 8), // "addParam"
+QT_MOC_LITERAL(5, 28, 9), // "inOutMode"
+QT_MOC_LITERAL(6, 38, 8), // "drawMap0"
+QT_MOC_LITERAL(7, 47, 8), // "drawMap1"
+QT_MOC_LITERAL(8, 56, 8), // "drawMap2"
+QT_MOC_LITERAL(9, 65, 8), // "drawMap3"
+QT_MOC_LITERAL(10, 74, 8), // "drawMap4"
+QT_MOC_LITERAL(11, 83, 8), // "drawMap5"
+QT_MOC_LITERAL(12, 92, 8), // "drawMap6"
+QT_MOC_LITERAL(13, 101, 8), // "drawMap7"
+QT_MOC_LITERAL(14, 110, 8), // "drawMap8"
+QT_MOC_LITERAL(15, 119, 8), // "drawMap9"
+QT_MOC_LITERAL(16, 128, 9), // "drawMap10"
+QT_MOC_LITERAL(17, 138, 9), // "drawMap11"
+QT_MOC_LITERAL(18, 148, 9), // "drawMap12"
+QT_MOC_LITERAL(19, 158, 9), // "drawMap13"
+QT_MOC_LITERAL(20, 168, 9), // "drawMap14"
+QT_MOC_LITERAL(21, 178, 9), // "drawMap15"
+QT_MOC_LITERAL(22, 188, 9), // "drawMap16"
+QT_MOC_LITERAL(23, 198, 9), // "drawMap17"
+QT_MOC_LITERAL(24, 208, 9), // "drawMap18"
+QT_MOC_LITERAL(25, 218, 9), // "drawMap19"
+QT_MOC_LITERAL(26, 228, 9), // "drawMap20"
+QT_MOC_LITERAL(27, 238, 9), // "drawMap21"
+QT_MOC_LITERAL(28, 248, 9), // "drawMap22"
+QT_MOC_LITERAL(29, 258, 9), // "drawMap23"
+QT_MOC_LITERAL(30, 268, 9), // "drawMap24"
+QT_MOC_LITERAL(31, 278, 9), // "drawMap25"
+QT_MOC_LITERAL(32, 288, 9), // "drawMap26"
+QT_MOC_LITERAL(33, 298, 9), // "drawMap27"
+QT_MOC_LITERAL(34, 308, 9), // "drawMap28"
+QT_MOC_LITERAL(35, 318, 9), // "drawMap29"
+QT_MOC_LITERAL(36, 328, 9), // "drawMap30"
+QT_MOC_LITERAL(37, 338, 9), // "drawMap31"
+QT_MOC_LITERAL(38, 348, 9), // "drawMap32"
+QT_MOC_LITERAL(39, 358, 9), // "drawMap33"
+QT_MOC_LITERAL(40, 368, 9), // "drawMap34"
+QT_MOC_LITERAL(41, 378, 9), // "drawMap35"
+QT_MOC_LITERAL(42, 388, 9), // "drawMap36"
+QT_MOC_LITERAL(43, 398, 18), // "showCountryCloseUp"
+QT_MOC_LITERAL(44, 417, 11), // "showHistory"
+QT_MOC_LITERAL(45, 429, 8), // "nextNews"
+QT_MOC_LITERAL(46, 438, 8), // "prevNews"
+QT_MOC_LITERAL(47, 447, 9), // "showNewsp"
+QT_MOC_LITERAL(48, 457, 8), // "showUSAA"
+QT_MOC_LITERAL(49, 466, 8), // "showUSAO"
+QT_MOC_LITERAL(50, 475, 9), // "showUSSRA"
+QT_MOC_LITERAL(51, 485, 9), // "showUSSRO"
+QT_MOC_LITERAL(52, 495, 7), // "showMIN"
+QT_MOC_LITERAL(53, 503, 8), // "doDestab"
+QT_MOC_LITERAL(54, 512, 9), // "doEconAid"
+QT_MOC_LITERAL(55, 522, 8), // "doMilAid"
+QT_MOC_LITERAL(56, 531, 8), // "doRebAid"
+QT_MOC_LITERAL(57, 540, 9), // "doMilIntv"
+QT_MOC_LITERAL(58, 550, 9), // "doRebIntv"
+QT_MOC_LITERAL(59, 560, 7), // "doPress"
+QT_MOC_LITERAL(60, 568, 8), // "doTreaty"
+QT_MOC_LITERAL(61, 577, 11), // "enactPolicy"
+QT_MOC_LITERAL(62, 589, 10), // "showScores"
+QT_MOC_LITERAL(63, 600, 11), // "showNewGame"
+QT_MOC_LITERAL(64, 612, 10), // "setNewGame"
+QT_MOC_LITERAL(65, 623, 10), // "doSaveGame"
+QT_MOC_LITERAL(66, 634, 10), // "doNextTurn"
+QT_MOC_LITERAL(67, 645, 16), // "continueNextTurn"
+QT_MOC_LITERAL(68, 662, 11), // "changeSides"
+QT_MOC_LITERAL(69, 674, 13), // "doCHumanLoose"
+QT_MOC_LITERAL(70, 688, 13), // "doCHumanTough"
+QT_MOC_LITERAL(71, 702, 7) // "doAbout"
+
+    },
+    "MainBox\0drawMap\0\0t\0addParam\0inOutMode\0"
+    "drawMap0\0drawMap1\0drawMap2\0drawMap3\0"
+    "drawMap4\0drawMap5\0drawMap6\0drawMap7\0"
+    "drawMap8\0drawMap9\0drawMap10\0drawMap11\0"
+    "drawMap12\0drawMap13\0drawMap14\0drawMap15\0"
+    "drawMap16\0drawMap17\0drawMap18\0drawMap19\0"
+    "drawMap20\0drawMap21\0drawMap22\0drawMap23\0"
+    "drawMap24\0drawMap25\0drawMap26\0drawMap27\0"
+    "drawMap28\0drawMap29\0drawMap30\0drawMap31\0"
+    "drawMap32\0drawMap33\0drawMap34\0drawMap35\0"
+    "drawMap36\0showCountryCloseUp\0showHistory\0"
+    "nextNews\0prevNews\0showNewsp\0showUSAA\0"
+    "showUSAO\0showUSSRA\0showUSSRO\0showMIN\0"
+    "doDestab\0doEconAid\0doMilAid\0doRebAid\0"
+    "doMilIntv\0doRebIntv\0doPress\0doTreaty\0"
+    "enactPolicy\0showScores\0showNewGame\0"
+    "setNewGame\0doSaveGame\0doNextTurn\0"
+    "continueNextTurn\0changeSides\0doCHumanLoose\0"
+    "doCHumanTough\0doAbout"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainBox[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      67,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    3,  349,    2, 0x08 /* Private */,
+       6,    0,  356,    2, 0x08 /* Private */,
+       7,    0,  357,    2, 0x08 /* Private */,
+       8,    0,  358,    2, 0x08 /* Private */,
+       9,    0,  359,    2, 0x08 /* Private */,
+      10,    0,  360,    2, 0x08 /* Private */,
+      11,    0,  361,    2, 0x08 /* Private */,
+      12,    0,  362,    2, 0x08 /* Private */,
+      13,    0,  363,    2, 0x08 /* Private */,
+      14,    0,  364,    2, 0x08 /* Private */,
+      15,    0,  365,    2, 0x08 /* Private */,
+      16,    0,  366,    2, 0x08 /* Private */,
+      17,    0,  367,    2, 0x08 /* Private */,
+      18,    0,  368,    2, 0x08 /* Private */,
+      19,    0,  369,    2, 0x08 /* Private */,
+      20,    0,  370,    2, 0x08 /* Private */,
+      21,    0,  371,    2, 0x08 /* Private */,
+      22,    0,  372,    2, 0x08 /* Private */,
+      23,    0,  373,    2, 0x08 /* Private */,
+      24,    0,  374,    2, 0x08 /* Private */,
+      25,    0,  375,    2, 0x08 /* Private */,
+      26,    0,  376,    2, 0x08 /* Private */,
+      27,    0,  377,    2, 0x08 /* Private */,
+      28,    0,  378,    2, 0x08 /* Private */,
+      29,    0,  379,    2, 0x08 /* Private */,
+      30,    0,  380,    2, 0x08 /* Private */,
+      31,    0,  381,    2, 0x08 /* Private */,
+      32,    0,  382,    2, 0x08 /* Private */,
+      33,    0,  383,    2, 0x08 /* Private */,
+      34,    0,  384,    2, 0x08 /* Private */,
+      35,    0,  385,    2, 0x08 /* Private */,
+      36,    0,  386,    2, 0x08 /* Private */,
+      37,    0,  387,    2, 0x08 /* Private */,
+      38,    0,  388,    2, 0x08 /* Private */,
+      39,    0,  389,    2, 0x08 /* Private */,
+      40,    0,  390,    2, 0x08 /* Private */,
+      41,    0,  391,    2, 0x08 /* Private */,
+      42,    0,  392,    2, 0x08 /* Private */,
+      43,    0,  393,    2, 0x08 /* Private */,
+      44,    0,  394,    2, 0x08 /* Private */,
+      45,    0,  395,    2, 0x08 /* Private */,
+      46,    0,  396,    2, 0x08 /* Private */,
+      47,    0,  397,    2, 0x08 /* Private */,
+      48,    0,  398,    2, 0x08 /* Private */,
+      49,    0,  399,    2, 0x08 /* Private */,
+      50,    0,  400,    2, 0x08 /* Private */,
+      51,    0,  401,    2, 0x08 /* Private */,
+      52,    0,  402,    2, 0x08 /* Private */,
+      53,    0,  403,    2, 0x08 /* Private */,
+      54,    0,  404,    2, 0x08 /* Private */,
+      55,    0,  405,    2, 0x08 /* Private */,
+      56,    0,  406,    2, 0x08 /* Private */,
+      57,    0,  407,    2, 0x08 /* Private */,
+      58,    0,  408,    2, 0x08 /* Private */,
+      59,    0,  409,    2, 0x08 /* Private */,
+      60,    0,  410,    2, 0x08 /* Private */,
+      61,    0,  411,    2, 0x08 /* Private */,
+      62,    0,  412,    2, 0x08 /* Private */,
+      63,    0,  413,    2, 0x08 /* Private */,
+      64,    0,  414,    2, 0x08 /* Private */,
+      65,    0,  415,    2, 0x08 /* Private */,
+      66,    0,  416,    2, 0x08 /* Private */,
+      67,    0,  417,    2, 0x08 /* Private */,
+      68,    0,  418,    2, 0x08 /* Private */,
+      69,    0,  419,    2, 0x08 /* Private */,
+      70,    0,  420,    2, 0x08 /* Private */,
+      71,    0,  421,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, QMetaType::Bool,    3,    4,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainBox::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainBox *_t = static_cast<MainBox *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->drawMap((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< bool(*)>(_a[3]))); break;
+        case 1: _t->drawMap0(); break;
+        case 2: _t->drawMap1(); break;
+        case 3: _t->drawMap2(); break;
+        case 4: _t->drawMap3(); break;
+        case 5: _t->drawMap4(); break;
+        case 6: _t->drawMap5(); break;
+        case 7: _t->drawMap6(); break;
+        case 8: _t->drawMap7(); break;
+        case 9: _t->drawMap8(); break;
+        case 10: _t->drawMap9(); break;
+        case 11: _t->drawMap10(); break;
+        case 12: _t->drawMap11(); break;
+        case 13: _t->drawMap12(); break;
+        case 14: _t->drawMap13(); break;
+        case 15: _t->drawMap14(); break;
+        case 16: _t->drawMap15(); break;
+        case 17: _t->drawMap16(); break;
+        case 18: _t->drawMap17(); break;
+        case 19: _t->drawMap18(); break;
+        case 20: _t->drawMap19(); break;
+        case 21: _t->drawMap20(); break;
+        case 22: _t->drawMap21(); break;
+        case 23: _t->drawMap22(); break;
+        case 24: _t->drawMap23(); break;
+        case 25: _t->drawMap24(); break;
+        case 26: _t->drawMap25(); break;
+        case 27: _t->drawMap26(); break;
+        case 28: _t->drawMap27(); break;
+        case 29: _t->drawMap28(); break;
+        case 30: _t->drawMap29(); break;
+        case 31: _t->drawMap30(); break;
+        case 32: _t->drawMap31(); break;
+        case 33: _t->drawMap32(); break;
+        case 34: _t->drawMap33(); break;
+        case 35: _t->drawMap34(); break;
+        case 36: _t->drawMap35(); break;
+        case 37: _t->drawMap36(); break;
+        case 38: _t->showCountryCloseUp(); break;
+        case 39: _t->showHistory(); break;
+        case 40: _t->nextNews(); break;
+        case 41: _t->prevNews(); break;
+        case 42: _t->showNewsp(); break;
+        case 43: _t->showUSAA(); break;
+        case 44: _t->showUSAO(); break;
+        case 45: _t->showUSSRA(); break;
+        case 46: _t->showUSSRO(); break;
+        case 47: _t->showMIN(); break;
+        case 48: _t->doDestab(); break;
+        case 49: _t->doEconAid(); break;
+        case 50: _t->doMilAid(); break;
+        case 51: _t->doRebAid(); break;
+        case 52: _t->doMilIntv(); break;
+        case 53: _t->doRebIntv(); break;
+        case 54: _t->doPress(); break;
+        case 55: _t->doTreaty(); break;
+        case 56: _t->enactPolicy(); break;
+        case 57: _t->showScores(); break;
+        case 58: _t->showNewGame(); break;
+        case 59: _t->setNewGame(); break;
+        case 60: _t->doSaveGame(); break;
+        case 61: _t->doNextTurn(); break;
+        case 62: _t->continueNextTurn(); break;
+        case 63: _t->changeSides(); break;
+        case 64: _t->doCHumanLoose(); break;
+        case 65: _t->doCHumanTough(); break;
+        case 66: _t->doAbout(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainBox::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainBox.data,
+      qt_meta_data_MainBox,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainBox::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainBox::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainBox.stringdata0))
+        return static_cast<void*>(const_cast< MainBox*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainBox::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 67)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 67;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 67)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 67;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
